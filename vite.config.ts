2ee2733a131d9d8961e0fa2ad6f760c94015d41a
MaxsanAI <maxsansamurai@gmail.com>
2026-09-05 06:49:47 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // <--- DODAJTE SAMO OVO DA NE BUDE BELA STRANICA
   plugins: [react()],
   resolve: {
     alias: {
